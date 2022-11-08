a9232b8e73f55f1c706dc01ce6868bd80b070357
Ermek Abilov <xji.xjp@gmail.com>
2022-11-08 19:43:47 +0600

    feature Create new project

---
+++ Animal-Protection/Animal-Protection/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Animal_Protection.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Animal_Protection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Animal-Protection/Animal-Protection/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Animal_Protection.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

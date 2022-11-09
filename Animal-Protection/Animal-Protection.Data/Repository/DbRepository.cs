7c9ffa9fdafc3042bbd3e669021a31c4c4229e78
Dastan <sotaimouto@gmail.com>
2022-11-09 17:02:33 +0600

    feature: Added IRepository and DbRepository

---
+++ Animal-Protection/Animal-Protection.Data/Repository/DbRepository.cs
@@ -0,0 +1,54 @@
+﻿using Animal_Protection.Data;
+using ProjectAnimal.Model.Repository;
+using Protection_Animal.Model.Entities;
+
+namespace StudentManager.Model.Repositories
+{
+    public class DbRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        protected readonly AppDbContext Ctx;
+
+        public DbRepository(AppDbContext ctx)
+        {
+            Ctx = ctx;
+        }
+        public T Create(T entity)
+        {
+            var a = Ctx.Set<T>().Add(entity);
+            Ctx.SaveChanges();
+
+            return a.Entity;
+        }
+        public List<T> ReadAll()
+        {
+            var list = Ctx.Set<T>().ToList();
+
+            return list;
+        }
+        public T ReadById(int id)
+        {
+            var entity = Ctx.Set<T>().FirstOrDefault(en => en.Id == id);
+            return entity;
+        }
+        public T Update(T entity)
+        {
+            var a = Ctx.Set<T>().Update(entity);
+            Ctx.SaveChanges();
+
+            return a.Entity;
+        }
+        public void Delete(T entity)
+        {
+            Ctx.Set<T>().Remove(entity);
+
+            Ctx.SaveChanges();
+        }
+        public void DeleteById(int id)
+        {
+            var entity = ReadById(id);
+            Ctx.Set<T>().Remove(entity);
+
+            Ctx.SaveChanges();
+        }
+    }
+}
---
+++ Animal-Protection/Animal-Protection.Data/Repository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using Protection_Animal.Model.Entities;
+
+namespace ProjectAnimal.Model.Repository
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        public T Create(T entity);
+        public T Update(T entity);
+        public List<T> ReadAll();
+        public T ReadById(int id);
+        public void Delete(T entity);
+        public void DeleteById(int id);
+
+    }
+}

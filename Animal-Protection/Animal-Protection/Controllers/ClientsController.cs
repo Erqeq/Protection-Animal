f1bc608321144a70c193bea8d3fda1043fb2806b
Dastan <sotaimouto@gmail.com>
2022-12-01 15:45:27 +0600

    fix: remove edit options from clients

--- Animal-Protection/Animal-Protection/Controllers/ClientsController.cs
+++ Animal-Protection/Animal-Protection/Controllers/ClientsController.cs
@@ -49,56 +49,7 @@ namespace Animal_Protection.Controllers
             }
             return View(client);
         }
-
-        // GET: Clients/Edit/5
-        public async Task<IActionResult> Edit(string id)
-        {
-            if (id == null || _context.Clients == null)
-            {
-                return NotFound();
-            }
-
-            var client = await _context.Clients.FindAsync(id);
-            if (client == null)
-            {
-                return NotFound();
-            }
-            return View(client);
-        }
-
-        // POST: Clients/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string id, [Bind("Id,Email,TelephoneNumber,Address,Name")] Client client)
-        {
-            if (id != client.Id)
-            {
-                return NotFound();
-            }
-
-            try
-            {
-                _context.Update(client);
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ClientExists(client.Id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-            return RedirectToAction(nameof(Index));
-
-            return View(client);
-        }
-
+ 
         // GET: Clients/Delete/5
         public async Task<IActionResult> Delete(string? id)
         {

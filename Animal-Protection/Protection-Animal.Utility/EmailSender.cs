9ff16e94ea7595b9cb3b2094bd76b1a6ee3c1ff6
Ermek Abilov <ermek.abilove@gmail.com>
2022-12-01 20:25:02 +0600

    Fix: fix category view

--- Animal-Protection/Protection-Animal.Utility/EmailSender.cs
+++ Animal-Protection/Protection-Animal.Utility/EmailSender.cs
@@ -1,11 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 using Mailjet.Client;
 using Mailjet.Client.Resources;
 using Newtonsoft.Json.Linq;
